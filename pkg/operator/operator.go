54028165b70dc55325ddaeae978e630f93249c1f
Filip Petkovski <filip.petkovsky@gmail.com>
2021-10-20 12:13:05 +0200

    test: run e2e tests in the CI against an in-cluster operator
    We currently spin up the operator in a goroutine when running e2e tests.
    This works well for the local environment but does not verify the actual
    behavior in a real Kubernetes cluster. For example, it does not take
    into account whether the operator service account has all of the
    required permissions.
    
    This commit adds a flag to exclude running the operator in a goroutine
    during e2e tests. It also changes the workflow in the CI to deploy the
    manifests so that we run the operator in-cluster and the e2e tests
    out-of-cluster. This way we can maintain a simpler workflow locally
    while having more reliable tests in the CI.

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -3,16 +3,10 @@ package operator
 import (
 	"context"
 	"fmt"
-	"rhobs/monitoring-stack-operator/pkg/apis/v1alpha1"
 	poctrl "rhobs/monitoring-stack-operator/pkg/controllers/prometheus-operator"
 
-	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
@@ -22,15 +16,8 @@ type Operator struct {
 }
 
 func New(metricsAddr string, poOpts poctrl.Options) (*Operator, error) {
-	scheme := runtime.NewScheme()
-
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(v1alpha1.AddToScheme(scheme))
-	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
-	utilruntime.Must(monitoringv1.AddToScheme(scheme))
-
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
+		Scheme:             NewScheme(),
 		MetricsBindAddress: metricsAddr,
 	})
 	if err != nil {
---
+++ pkg/operator/scheme.go
@@ -0,0 +1,22 @@
+package operator
+
+import (
+	"rhobs/monitoring-stack-operator/pkg/apis/v1alpha1"
+
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+)
+
+func NewScheme() *runtime.Scheme {
+	scheme := runtime.NewScheme()
+
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
+	utilruntime.Must(monitoringv1.AddToScheme(scheme))
+
+	return scheme
+}
