50e261de001e27ee7d6c1211c3bf6927cfb3f0e0
Sunil Thaha <sthaha@redhat.com>
2022-10-20 17:21:54 +1000

    feat: deploy admission-webhook
    This commit adds admission-webhook deployment and Validating Webhook Configurations
    for monitoring.rhobs PrometheusRule and AlertmanagerConfig.
    
    Signed-off-by: Sunil Thaha <sthaha@redhat.com>

---
+++ test/e2e/po_admission_webhook_test.go
@@ -0,0 +1,66 @@
+package e2e
+
+import (
+	"context"
+	"testing"
+
+	monv1 "github.com/rhobs/obo-prometheus-operator/pkg/apis/monitoring/v1"
+	"gotest.tools/v3/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func TestPrometheusRuleWebhook(t *testing.T) {
+	assertCRDExists(t,
+		"prometheusrules.monitoring.rhobs",
+	)
+	ts := []testCase{{
+		name:     "Valid PrometheusRules are accepted",
+		scenario: validPrometheusRuleIsAccepted,
+	}, {
+		name:     "Valid PrometheusRules are accepted",
+		scenario: invalidPrometheusRuleIsRejected,
+	}}
+
+	for _, tc := range ts {
+		t.Run(tc.name, tc.scenario)
+	}
+}
+
+func validPrometheusRuleIsAccepted(t *testing.T) {
+	rule := newSinglePrometheusRule(t, "valid-rule",
+		`increase(controller_runtime_reconcile_errors_total{job="foobar"}[15m]) > 0`,
+	)
+	err := f.K8sClient.Create(context.Background(), rule)
+	assert.NilError(t, err, `failed to create a valid log`)
+}
+
+func invalidPrometheusRuleIsRejected(t *testing.T) {
+	rule := newSinglePrometheusRule(t, "valid-rule", `FOOBAR({job="foobar"}[15m]) > 0`)
+	err := f.K8sClient.Create(context.Background(), rule)
+	assert.ErrorContains(t, err, `denied the request: Rules are not valid`)
+}
+
+func newSinglePrometheusRule(t *testing.T, name, expr string) *monv1.PrometheusRule {
+	rule := &monv1.PrometheusRule{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: e2eTestNamespace,
+		},
+		Spec: monv1.PrometheusRuleSpec{
+			Groups: []monv1.RuleGroup{{
+				Name: "single-rule-group",
+				Rules: []monv1.Rule{{
+					Alert: "alert name",
+					Expr:  intstr.FromString(expr),
+					For:   "15m",
+				}},
+			}},
+		},
+	}
+	f.CleanUp(t, func() {
+		f.K8sClient.Delete(context.Background(), rule)
+	})
+
+	return rule
+}
