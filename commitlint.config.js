b268ce50d3220ae7b683299eef48a7843b00fb60
Simon Pasquier <spasquie@redhat.com>
2023-08-16 11:01:35 +0200

    build: add commitlint exception for Dependabot (#331)
    Dependabot commit messages are prefixed by `build(deps):`. Adding an
    exception for the `build` prefix.
    
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Ignore dependabot commit messages until https://github.com/dependabot/dependabot-core/issues/2445 is fixed.
+   */
+  ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
+};
+
+module.exports = Configuration;
