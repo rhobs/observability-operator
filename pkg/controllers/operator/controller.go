5f2b6e5eb86a4186d80ae56d5957606118d10cf0
Jan Fajerski <jan--f@users.noreply.github.com>
2024-11-05 18:29:09 +0100

    feat: add operator controller to add ServiceMonitor (#616)
    * feat: add operator controller to add ServiceMonitor
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>
    
    * chore: address review comments
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>
    
    ---------
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

---
+++ pkg/controllers/operator/controller.go
@@ -0,0 +1,127 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package operator
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	monv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type resourceManager struct {
+	k8sClient  client.Client
+	scheme     *runtime.Scheme
+	logger     logr.Logger
+	controller controller.Controller
+	namespace  string
+}
+
+// RBAC for managing Prometheus Operator CRs
+//+kubebuilder:rbac:groups=monitoring.coreos.com,resources=servicemonitors,verbs=list;watch;create;update;delete;patch
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles;rolebindings,verbs=list;create;update;patch
+
+// RegisterWithManager registers the controller with Manager
+func RegisterWithManager(mgr ctrl.Manager, namespace string) error {
+
+	rm := &resourceManager{
+		k8sClient: mgr.GetClient(),
+		scheme:    mgr.GetScheme(),
+		logger:    ctrl.Log.WithName("observability-operator"),
+		namespace: namespace,
+	}
+	// We only want to trigger a reconciliation when the generation
+	// of a child changes. Until we need to update our the status for our own objects,
+	// we can save CPU cycles by avoiding reconciliations triggered by
+	// child status changes.
+	generationChanged := builder.WithPredicates(predicate.GenerationChangedPredicate{})
+
+	ctrl, err := ctrl.NewControllerManagedBy(mgr).
+		Owns(&monv1.ServiceMonitor{}, generationChanged).
+		Watches(
+			&corev1.Service{},
+			handler.EnqueueRequestsFromMapFunc(rm.operatorService),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{}),
+		).
+		Build(rm)
+
+	if err != nil {
+		return err
+	}
+	rm.controller = ctrl
+	return nil
+}
+
+func (rm resourceManager) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := rm.logger.WithValues("operator", req.NamespacedName)
+	logger.Info("Reconciling operator resources")
+
+	op := &corev1.Service{}
+	err := rm.k8sClient.Get(ctx, req.NamespacedName, op)
+	if errors.IsNotFound(err) {
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	reconcilers := operatorComponentReconcilers(op, rm.namespace)
+	for _, reconciler := range reconcilers {
+		err := reconciler.Reconcile(ctx, rm.k8sClient, rm.scheme)
+		// handle create / update errors that can happen due to a stale cache by
+		// retrying after some time.
+		if errors.IsAlreadyExists(err) || errors.IsConflict(err) {
+			logger.V(3).Info("skipping reconcile error", "err", err)
+			return ctrl.Result{RequeueAfter: 2 * time.Second}, nil
+		}
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+func (rm resourceManager) operatorService(ctx context.Context, _ client.Object) []reconcile.Request {
+	var requests []reconcile.Request
+	op := &corev1.Service{}
+	err := rm.k8sClient.Get(ctx, types.NamespacedName{Name: "observability-operator", Namespace: rm.namespace}, op)
+	if errors.IsNotFound(err) {
+		return requests
+	}
+	if err != nil {
+		return requests
+	}
+	requests = append(requests, reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      op.GetName(),
+			Namespace: op.GetNamespace(),
+		},
+	})
+	return requests
+}
--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -23,6 +23,7 @@ import (
 
 	stackctrl "github.com/rhobs/observability-operator/pkg/controllers/monitoring/monitoring-stack"
 	tqctrl "github.com/rhobs/observability-operator/pkg/controllers/monitoring/thanos-querier"
+	opctrl "github.com/rhobs/observability-operator/pkg/controllers/operator"
 	uictrl "github.com/rhobs/observability-operator/pkg/controllers/uiplugin"
 )
 
@@ -250,6 +251,15 @@ func New(ctx context.Context, cfg *OperatorConfiguration) (*Operator, error) {
 	} else {
 		setupLog := ctrl.Log.WithName("setup")
 		setupLog.Info("OpenShift feature gate is disabled, UIPlugins are not enabled")
+	}
+
+	if cfg.FeatureGates.OpenShift.Enabled {
+		if err := opctrl.RegisterWithManager(mgr, cfg.Namespace); err != nil {
+			return nil, fmt.Errorf("unable to register operator controller: %w", err)
+		}
+	} else {
+		setupLog := ctrl.Log.WithName("setup")
+		setupLog.Info("OpenShift feature gate is disabled, Operator controller is not enabled")
 	}
 
 	if err := mgr.AddHealthzCheck("health probe", healthz.Ping); err != nil {
