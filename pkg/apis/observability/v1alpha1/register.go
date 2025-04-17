f94d839db36203883b8d8e41222e807a74a7ea81
Tomáš Remeš <tremes@redhat.com>
2025-04-17 07:49:02 +0200

    feat: remove unnecessary API dependency

--- pkg/apis/observability/v1alpha1/register.go
+++ pkg/apis/observability/v1alpha1/register.go
@@ -3,8 +3,9 @@
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
@@ -12,12 +13,14 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "observability.openshift.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = runtime.NewSchemeBuilder(addTypes)
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
 
-func init() {
-	SchemeBuilder.Register(&ClusterObservability{}, &ClusterObservabilityList{})
+func addTypes(s *runtime.Scheme) error {
+	s.AddKnownTypes(GroupVersion, &ClusterObservability{}, &ClusterObservabilityList{})
+	metav1.AddToGroupVersion(s, GroupVersion)
+	return nil
 }
