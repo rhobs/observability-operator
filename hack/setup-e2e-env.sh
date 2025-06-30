1c03dac52eeb0acb1799ba96268edadb79af3c6f
Jan Fajerski <jfajersk@redhat.com>
2025-06-30 17:10:47 +0200

    fix: add actions specific setup code
    This commit add detection if we run in github actions and add some
    actions specific code. Echoing kubeconf into github_env makes is
    available for following steps and adding the local registry to
    /etc/hosts is a workaround for actions specific local networking
    limitations.
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

--- hack/setup-e2e-env.sh
+++ hack/setup-e2e-env.sh
@@ -349,6 +349,10 @@ setup_cluster() {
     kubectl wait --for=condition=Ready nodes --all --timeout=300s
     kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s
 
+    # export $KUBECONFIG so its available for other steps
+    if [[ "$GITHUB_ACTIONS" ]]; then
+        echo "KUBECONFIG=$KUBECONFIG" >> "$GITHUB_ENV"
+    fi
     ok "Kind cluster is ready"
 }
 
@@ -405,6 +409,11 @@ setup_registry() {
     kubectl apply -f "$SCRIPT_DIR/kind/registry.yaml" -n operators
     kubectl rollout status deployment local-registry -n operators --timeout=300s
     kubectl wait --for=condition=Available deploy local-registry -n operators --timeout=300s
+    # add local-registry to /etc/hosts in case we run in a github action
+    if [[ "$GITHUB_ACTIONS" ]]; then
+        echo "Detected github actions run, adding \"127.0.0.1 local-registry\" to \"/etc/hosts\""
+        echo "127.0.0.1 local-registry" | sudo tee -a /etc/hosts
+    fi
 
     # Test registry connectivity
     local max_attempts=10
