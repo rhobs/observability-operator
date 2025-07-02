adeb53be7ec2f057ead8282d8af818152ffdefb5
Pavol Loffay <p.loffay@gmail.com>
2025-07-02 21:49:54 +0200

    feat: add controller for clusterobservability (#793)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

---
+++ pkg/controllers/observability/observability_controller.go
@@ -0,0 +1,304 @@
+package observability
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-logr/logr"
+	olmv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	tempov1alpha1 "github.com/grafana/tempo-operator/api/tempo/v1alpha1"
+	otelv1beta1 "github.com/open-telemetry/opentelemetry-operator/apis/v1beta1"
+	obsv1alpha1 "github.com/rhobs/observability-operator/pkg/apis/observability/v1alpha1"
+	uiv1alpha1 "github.com/rhobs/observability-operator/pkg/apis/uiplugin/v1alpha1"
+)
+
+const (
+	finalizerName = "observability.openshift.io/clusterobservability"
+
+	conditionReasonError    = "ReconcileError"
+	conditionTypeReconciled = "Reconciled"
+)
+
+// RBAC for the ClusterObservability CRD
+// +kubebuilder:rbac:groups=observability.openshift.io,resources=clusterobservabilities,verbs=get;list;watch;create;update;delete;patch
+// +kubebuilder:rbac:groups=observability.openshift.io,resources=clusterobservabilities/status;clusterobservabilities/finalizers,verbs=get;update;delete;patch
+
+// RBAC for installing operators
+// +kubebuilder:rbac:groups=operators.coreos.com,resources=subscriptions,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=operators.coreos.com,resources=clusterserviceversions,verbs=get;list;watch;create;update;patch;delete
+
+// RBAC for OTEL
+// +kubebuilder:rbac:groups=opentelemetry.io,resources=opentelemetrycollectors,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=opentelemetry.io,resources=opentelemetrycollectors/status,verbs=get;list;watch
+
+// RBAC for Tempo
+// +kubebuilder:rbac:groups=tempo.grafana.com,resources=tempostacks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=tempo.grafana.com,resources=tempostacks/status,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=secrets;configmaps,verbs=get;list;watch;create;update;delete;patch
+// +kubebuilder:rbac:groups=observability.openshift.io,resources=uiplugins,verbs=get;list;watch;create;update;delete;patch
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles;clusterrolebindings,verbs=list;watch;create;update;delete;patch
+// +kubebuilder:rbac:groups=observability.openshift.io,resources=uiplugins,verbs=get;list;watch;create;update;delete;patch
+// +kubebuilder:rbac:groups=tempo.grafana.com,resources=application,resourceNames=traces,verbs=create
+
+type clusterObservabilityController struct {
+	client          client.Client
+	scheme          *runtime.Scheme
+	logger          logr.Logger
+	Options         Options
+	controller      controller.TypedController[reconcile.Request]
+	cache           cache.Cache
+	discoveryClient *discovery.DiscoveryClient
+	watchOTELcol    *sync.Once
+	watchTempo      *sync.Once
+}
+
+var _ reconcile.TypedReconciler[reconcile.Request] = (*clusterObservabilityController)(nil)
+
+func (o clusterObservabilityController) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	o.logger.Info("Reconcile called", "request", request)
+
+	instance, err := o.getInstance(ctx, request)
+	if err != nil {
+		// retry since some error has occurred
+		return ctrl.Result{}, err
+	}
+	if instance == nil {
+		return ctrl.Result{}, nil
+	}
+
+	// Add finalizer for this CR
+	if !controllerutil.ContainsFinalizer(instance, finalizerName) {
+		if controllerutil.AddFinalizer(instance, finalizerName) {
+			err := o.client.Update(ctx, instance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	csvs := &olmv1alpha1.ClusterServiceVersionList{}
+	err = o.client.List(ctx, csvs, &client.ListOptions{Namespace: o.Options.COONamespace})
+	if err != nil {
+		o.logger.Error(err, "Failed to list csvs")
+		return ctrl.Result{}, err
+	}
+	subs := &olmv1alpha1.SubscriptionList{}
+	err = o.client.List(ctx, subs, &client.ListOptions{})
+	if err != nil {
+		o.logger.Error(err, "Failed to list subscriptions")
+		return ctrl.Result{}, err
+	}
+	reconcilers, err := getReconcilers(ctx, o.client, instance, o.Options, operatorsStatus{
+		cooNamespace: o.Options.COONamespace,
+		csvs:         csvs.Items,
+		subs:         subs.Items,
+	})
+	if err != nil {
+		o.logger.Error(err, "Failed to get reconcilers")
+		return ctrl.Result{}, err
+	}
+	for _, reconciler := range reconcilers {
+		reconcileErr := reconciler.Reconcile(ctx, o.client, o.scheme)
+		// handle creation / update errors that can happen due to a stale cache by
+		// retrying after some time.
+		if apierrors.IsAlreadyExists(err) || apierrors.IsConflict(err) {
+			o.logger.V(1).Info("skipping reconcile error", "err", reconcileErr)
+			return ctrl.Result{RequeueAfter: 2 * time.Second}, nil
+		}
+		if reconcileErr != nil {
+			o.logger.Error(reconcileErr, "Failed to reconcile")
+			return o.updateStatus(ctx, instance, err), err
+		}
+	}
+
+	groups, err := o.discoveryClient.ServerGroups()
+	if err != nil {
+		o.logger.Error(err, "Failed to get server groups / CDRs")
+		return ctrl.Result{}, fmt.Errorf("failed to get server groups: %w", err)
+	}
+	for _, group := range groups.Groups {
+		if group.Name == "opentelemetry.io" {
+			o.watchOTELcol.Do(func() {
+				if err := o.controller.Watch(source.Kind[client.Object](o.cache, &otelv1beta1.OpenTelemetryCollector{}, handler.EnqueueRequestsFromMapFunc(o.triggerReconcile))); err != nil {
+					o.logger.Error(err, "Failed to watch OpenTelemetryCollector resources")
+				}
+			})
+		}
+
+		if group.Name == "tempo.grafana.com" {
+			o.watchTempo.Do(func() {
+				if err := o.controller.Watch(source.Kind[client.Object](o.cache, &tempov1alpha1.TempoStack{}, handler.EnqueueRequestsFromMapFunc(o.triggerReconcile))); err != nil {
+					o.logger.Error(err, "Failed to watch TempoStack resources")
+				}
+			})
+		}
+	}
+
+	// We have a deletion, short circuit and let the deletion happen
+	if instance.ObjectMeta.DeletionTimestamp != nil {
+		if controllerutil.ContainsFinalizer(instance, finalizerName) {
+			// Once all finalizers have been
+			// removed, the object will be deleted.
+			if controllerutil.RemoveFinalizer(instance, finalizerName) {
+				err := o.client.Update(ctx, instance)
+				if err != nil {
+					return ctrl.Result{}, err
+				}
+			}
+		}
+	}
+
+	return o.updateStatus(ctx, instance, nil), nil
+}
+
+func (o clusterObservabilityController) triggerReconcile(ctx context.Context, _ client.Object) []reconcile.Request {
+	instances := &obsv1alpha1.ClusterObservabilityList{}
+	listOps := &client.ListOptions{}
+	err := o.client.List(ctx, instances, listOps)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(instances.Items))
+	for i, item := range instances.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      item.GetName(),
+				Namespace: item.GetNamespace(),
+			},
+		}
+	}
+	return requests
+}
+
+func (o clusterObservabilityController) getInstance(ctx context.Context, req ctrl.Request) (*obsv1alpha1.ClusterObservability, error) {
+	instance := obsv1alpha1.ClusterObservability{}
+	if err := o.client.Get(ctx, req.NamespacedName, &instance); err != nil {
+		if apierrors.IsNotFound(err) {
+			o.logger.V(3).Info("instance could not be found; may be marked for deletion")
+			return nil, nil
+		}
+		o.logger.Error(err, "failed to get cluster observability instance")
+		return nil, err
+	}
+
+	return &instance, nil
+}
+
+func (o clusterObservabilityController) updateStatus(ctx context.Context, instance *obsv1alpha1.ClusterObservability, reconcileErr error) reconcile.Result {
+	if instance.Spec.Capabilities != nil {
+		capabilities := instance.Spec.Capabilities
+		if capabilities.Tracing.Enabled {
+			otelcol := &otelv1beta1.OpenTelemetryCollector{}
+			err := o.client.Get(ctx, types.NamespacedName{
+				Namespace: instance.Namespace,
+				Name:      otelCollectorName(instance.Name),
+			}, otelcol)
+			if err != nil {
+				return ctrl.Result{RequeueAfter: 2 * time.Second}
+			}
+			tempo := &tempov1alpha1.TempoStack{}
+			err = o.client.Get(ctx, types.NamespacedName{
+				Namespace: instance.Namespace,
+				Name:      tempoName(instance.Name),
+			}, tempo)
+			if err != nil {
+				return ctrl.Result{RequeueAfter: 2 * time.Second}
+			}
+
+			instance.Status.Tempo = fmt.Sprintf("%s/%s (%s)", instance.Namespace, tempoName(instance.Name), tempo.Status.TempoVersion)
+			instance.Status.OpenTelemetry = fmt.Sprintf("%s/%s (%s)", instance.Namespace, otelCollectorName(instance.Name), otelcol.Status.Version)
+		}
+	} else {
+		instance.Status.Tempo = ""
+		instance.Status.OpenTelemetry = ""
+	}
+
+	if reconcileErr != nil {
+		instance.Status.Conditions = []metav1.Condition{
+			{
+				Reason:             conditionReasonError,
+				Type:               conditionTypeReconciled,
+				Status:             metav1.ConditionFalse,
+				Message:            reconcileErr.Error(),
+				LastTransitionTime: metav1.Now(),
+				ObservedGeneration: instance.GetGeneration(),
+			},
+		}
+	}
+
+	err := o.client.Status().Update(ctx, instance)
+	if err != nil {
+		o.logger.Error(err, "failed to update status")
+		return ctrl.Result{RequeueAfter: 2 * time.Second}
+	}
+
+	return ctrl.Result{}
+}
+
+type Options struct {
+	COONamespace          string
+	OpenTelemetryOperator OperatorInstallConfig
+	TempoOperator         OperatorInstallConfig
+}
+
+type OperatorInstallConfig struct {
+	Namespace   string
+	PackageName string
+	StartingCSV string
+	Channel     string
+}
+
+func RegisterWithManager(mgr ctrl.Manager, opts Options) error {
+	logger := ctrl.Log.WithName("cluster-observability")
+
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(mgr.GetConfig())
+	if err != nil {
+		return fmt.Errorf("failed to create discovery client: %w", err)
+	}
+
+	controller := &clusterObservabilityController{
+		client:          mgr.GetClient(),
+		scheme:          mgr.GetScheme(),
+		logger:          logger,
+		Options:         opts,
+		watchOTELcol:    &sync.Once{},
+		watchTempo:      &sync.Once{},
+		discoveryClient: discoveryClient,
+		cache:           mgr.GetCache(),
+	}
+
+	ctrlBuilder := ctrl.NewControllerManagedBy(mgr).
+		For(&obsv1alpha1.ClusterObservability{}).
+		Owns(&olmv1alpha1.Subscription{}).
+		Owns(&corev1.Secret{}).
+		Owns(&corev1.Namespace{}).
+		Owns(&uiv1alpha1.UIPlugin{}).
+		Owns(&rbacv1.ClusterRole{}).
+		Owns(&rbacv1.ClusterRoleBinding{}).
+		Named("cluster-observability")
+
+	controller.controller, err = ctrlBuilder.Build(controller)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ pkg/controllers/observability/tempo_components.go
@@ -0,0 +1,301 @@
+package observability
+
+import (
+	"context"
+	"fmt"
+
+	tempov1alpha1 "github.com/grafana/tempo-operator/api/tempo/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	obsv1alpha1 "github.com/rhobs/observability-operator/pkg/apis/observability/v1alpha1"
+	uiv1alpha1 "github.com/rhobs/observability-operator/pkg/apis/uiplugin/v1alpha1"
+)
+
+const (
+	tenantName = "application"
+	tenantID   = "1610b0c3-c509-4592-a256-a1871353dbfb"
+)
+
+func tempoStack(instance *obsv1alpha1.ClusterObservability) *tempov1alpha1.TempoStack {
+	tempo := &tempov1alpha1.TempoStack{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "TempoStack",
+			APIVersion: tempov1alpha1.GroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tempoName(instance.Name),
+			Namespace: instance.Namespace,
+		},
+		Spec: tempov1alpha1.TempoStackSpec{
+			Storage: tempov1alpha1.ObjectStorageSpec{
+				Secret: tempov1alpha1.ObjectStorageSecretSpec{
+					Type:           toTempoStorageType(instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec),
+					CredentialMode: toTempoCredentialMode(instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec),
+					Name:           tempoSecretName(instance.Name),
+				},
+			},
+			Template: tempov1alpha1.TempoTemplateSpec{
+				Gateway: tempov1alpha1.TempoGatewaySpec{
+					Enabled: true,
+				},
+			},
+			Tenants: &tempov1alpha1.TenantsSpec{
+				Mode: tempov1alpha1.ModeOpenShift,
+				Authentication: []tempov1alpha1.AuthenticationSpec{
+					{
+						TenantName: tenantName,
+						TenantID:   tenantID,
+					},
+				},
+			},
+		},
+	}
+
+	if instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec.TLS != nil {
+		tempo.Spec.Storage.TLS = tempov1alpha1.TLSSpec{
+			Enabled:    true,
+			CA:         tempoStorageCAConfigMapName(instance.Name),
+			Cert:       tempoStorageSecretName(instance.Name),
+			MinVersion: instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec.TLS.MinVersion,
+		}
+	}
+
+	return tempo
+}
+
+func tempoName(instance string) string {
+	return fmt.Sprintf("%s", instance)
+}
+
+func tempoStorageCAConfigMapName(name string) string {
+	return fmt.Sprintf("coo-%s-tempo-storage-ca", name)
+}
+
+func tempoStorageSecretName(name string) string {
+	return fmt.Sprintf("coo-%s-tempo-storage-cert", name)
+}
+
+func tempoSecretName(name string) string {
+	return fmt.Sprintf("coo-%s-tempo", name)
+}
+
+type tempoSecrets struct {
+	objectStorage *corev1.Secret
+
+	objectStorageTLSSecret   *corev1.Secret
+	objectStorageCAConfigMap *corev1.ConfigMap
+}
+
+func tempoStackSecrets(ctx context.Context, k8sClient client.Client, instance obsv1alpha1.ClusterObservability) (*tempoSecrets, error) {
+	var objectStorageCAConfMap *corev1.ConfigMap
+	var objectStorageTLSSecret *corev1.Secret
+
+	if instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec.TLS != nil {
+		tlsSpec := instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec.TLS
+		if tlsSpec.CAConfigMap != nil {
+			caConfigMap := &corev1.ConfigMap{}
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: instance.Namespace,
+				Name:      tlsSpec.CAConfigMap.Name,
+			}, caConfigMap)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get object storage CA configmap %s: %w", instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec.TLS.CAConfigMap.Name, err)
+			}
+
+			objectStorageCAConfMap = &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: corev1.SchemeGroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      tempoStorageCAConfigMapName(instance.Name),
+					Namespace: instance.Namespace,
+				},
+				Data: map[string]string{
+					"service-ca.crt": caConfigMap.Data[tlsSpec.CAConfigMap.Key],
+				},
+			}
+		}
+
+		if tlsSpec.CertSecret != nil {
+			certSecret := &corev1.Secret{}
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: instance.Namespace,
+				Name:      tlsSpec.CertSecret.Name,
+			}, certSecret)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get object storage cert secret %s: %w", tlsSpec.CertSecret.Name, err)
+			}
+
+			objectStorageTLSSecret = &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Secret",
+					APIVersion: corev1.SchemeGroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      tempoSecretName(instance.Name),
+					Namespace: instance.Namespace,
+				},
+			}
+			objectStorageTLSSecret.Data["tls.crt"] = certSecret.Data[tlsSpec.CertSecret.Key]
+		}
+		if tlsSpec.KeySecret != nil {
+			certSecret := &corev1.Secret{}
+			err := k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: instance.Namespace,
+				Name:      tlsSpec.KeySecret.Name,
+			}, certSecret)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get object storage cert secret %s: %w", tlsSpec.KeySecret.Name, err)
+			}
+
+			objectStorageTLSSecret.Data["tls.key"] = certSecret.Data[tlsSpec.KeySecret.Key]
+		}
+	}
+
+	tempoSecret := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: corev1.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tempoSecretName(instance.Name),
+			Namespace: instance.Namespace,
+		},
+	}
+	objectStorageSpec := instance.Spec.Capabilities.Tracing.Storage.ObjectStorageSpec
+	if objectStorageSpec.S3 != nil {
+		accessKeySecret := &corev1.Secret{}
+		err := k8sClient.Get(ctx, client.ObjectKey{
+			Namespace: instance.Namespace,
+			Name:      objectStorageSpec.S3.AccessKeySecret.Name,
+		}, accessKeySecret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get S3 access key secret %s: %w", objectStorageSpec.S3.AccessKeySecret.Name, err)
+		}
+
+		tempoSecret.Data = map[string][]byte{
+			"bucket":            []byte(objectStorageSpec.S3.Bucket),
+			"endpoint":          []byte(objectStorageSpec.S3.Endpoint),
+			"access_key_id":     []byte(objectStorageSpec.S3.AccessKeyID),
+			"access_key_secret": accessKeySecret.Data[objectStorageSpec.S3.AccessKeySecret.Key],
+		}
+
+		if objectStorageSpec.S3.Region != "" {
+			tempoSecret.Data["region"] = []byte(objectStorageSpec.S3.Region)
+		}
+	} else if objectStorageSpec.S3STS != nil {
+		tempoSecret.Data = map[string][]byte{
+			"bucket":   []byte(objectStorageSpec.S3STS.Bucket),
+			"role_arn": []byte(objectStorageSpec.S3STS.RoleARN),
+			"region":   []byte(objectStorageSpec.S3STS.RoleARN),
+		}
+	} else if objectStorageSpec.S3CCO != nil {
+		tempoSecret.Data = map[string][]byte{
+			"bucket": []byte(objectStorageSpec.S3CCO.Bucket),
+			"region": []byte(objectStorageSpec.S3CCO.Region),
+		}
+	} else if objectStorageSpec.Azure != nil {
+		accountKeySecret := &corev1.Secret{}
+		err := k8sClient.Get(ctx, client.ObjectKey{
+			Namespace: instance.Namespace,
+			Name:      objectStorageSpec.Azure.AccountKeySecret.Name,
+		}, accountKeySecret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get Azure account key secret %s: %w", objectStorageSpec.Azure.AccountKeySecret.Name, err)
+		}
+
+		tempoSecret.Data = map[string][]byte{
+			"container":    []byte(objectStorageSpec.Azure.Container),
+			"account_name": []byte(objectStorageSpec.Azure.AccountName),
+			"account_key":  accountKeySecret.Data[objectStorageSpec.Azure.AccountKeySecret.Key],
+		}
+	} else if objectStorageSpec.AzureWIF != nil {
+		tempoSecret.Data = map[string][]byte{
+			"container":    []byte(objectStorageSpec.AzureWIF.Container),
+			"account_name": []byte(objectStorageSpec.AzureWIF.AccountName),
+			"client_id":    []byte(objectStorageSpec.AzureWIF.ClientID),
+			"tenant_id":    []byte(objectStorageSpec.AzureWIF.TenantID),
+		}
+		if objectStorageSpec.AzureWIF.Audience != "" {
+			tempoSecret.Data["audience"] = []byte(objectStorageSpec.AzureWIF.Audience)
+		}
+	} else if objectStorageSpec.GCS != nil {
+		keyJSONSecret := &corev1.Secret{}
+		err := k8sClient.Get(ctx, client.ObjectKey{
+			Namespace: instance.Namespace,
+			Name:      objectStorageSpec.GCS.KeyJSONSecret.Name,
+		}, keyJSONSecret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get GCS keyJSON secret %s: %w", objectStorageSpec.GCS.KeyJSONSecret.Name, err)
+		}
+
+		tempoSecret.Data = map[string][]byte{
+			"bucketname": []byte(objectStorageSpec.GCS.Bucket),
+			"key.json":   keyJSONSecret.Data[objectStorageSpec.GCS.KeyJSONSecret.Key],
+		}
+	} else if objectStorageSpec.GCSSTSSpec != nil {
+		keyJSONSecret := &corev1.Secret{}
+		err := k8sClient.Get(ctx, client.ObjectKey{
+			Namespace: instance.Namespace,
+			Name:      objectStorageSpec.GCSSTSSpec.KeyJSONSecret.Name,
+		}, keyJSONSecret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get GCSSTS keyJSON secret %s: %w", objectStorageSpec.GCSSTSSpec.KeyJSONSecret.Name, err)
+		}
+
+		tempoSecret.Data = map[string][]byte{
+			"bucketname": []byte(objectStorageSpec.GCSSTSSpec.Bucket),
+			"key.json":   keyJSONSecret.Data[objectStorageSpec.GCSSTSSpec.KeyJSONSecret.Key],
+		}
+		if objectStorageSpec.GCSSTSSpec.Audience != "" {
+			tempoSecret.Data["audience"] = []byte(objectStorageSpec.GCSSTSSpec.Audience)
+		}
+	}
+
+	return &tempoSecrets{
+		objectStorage:            tempoSecret,
+		objectStorageTLSSecret:   objectStorageTLSSecret,
+		objectStorageCAConfigMap: objectStorageCAConfMap,
+	}, nil
+}
+
+func uiPlugin() *uiv1alpha1.UIPlugin {
+	return &uiv1alpha1.UIPlugin{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "UIPlugin",
+			APIVersion: uiv1alpha1.GroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "distributed-tracing",
+		},
+		Spec: uiv1alpha1.UIPluginSpec{
+			Type: uiv1alpha1.TypeDistributedTracing,
+		},
+	}
+}
+
+func toTempoStorageType(objStorage obsv1alpha1.TracingObjectStorageSpec) tempov1alpha1.ObjectStorageSecretType {
+	if objStorage.S3 != nil || objStorage.S3STS != nil || objStorage.S3CCO != nil {
+		return tempov1alpha1.ObjectStorageSecretS3
+	} else if objStorage.Azure != nil || objStorage.AzureWIF != nil {
+		return tempov1alpha1.ObjectStorageSecretAzure
+	} else if objStorage.GCS != nil || objStorage.GCSSTSSpec != nil {
+		return tempov1alpha1.ObjectStorageSecretGCS
+	}
+	return ""
+}
+
+func toTempoCredentialMode(objStorage obsv1alpha1.TracingObjectStorageSpec) tempov1alpha1.CredentialMode {
+	if objStorage.S3 != nil || objStorage.Azure != nil || objStorage.GCS != nil {
+		return tempov1alpha1.CredentialModeStatic
+	} else if objStorage.S3STS != nil || objStorage.AzureWIF != nil || objStorage.GCSSTSSpec != nil {
+		return tempov1alpha1.CredentialModeToken
+	} else if objStorage.S3CCO != nil {
+		return tempov1alpha1.CredentialModeTokenCCO
+	}
+
+	return ""
+}
