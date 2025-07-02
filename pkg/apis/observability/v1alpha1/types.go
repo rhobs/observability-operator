34bf82b9acfb070a9c23275a102f211635531538
Pavol Loffay <p.loffay@gmail.com>
2025-07-02 21:49:54 +0200

    feat: add controller for clusterobservability (#793)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/apis/observability/v1alpha1/types.go
+++ pkg/apis/observability/v1alpha1/types.go
@@ -1,6 +1,4 @@
 // +groupName=observability.openshift.io
-// +kubebuilder:rbac:groups=observability.openshift.io,resources=clusterobservability,verbs=list;get;watch
-// +kubebuilder:rbac:groups=observability.openshift.io,resources=clusterobservability/status;clusterobservability/finalizers,verbs=get;update
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 package v1alpha1
@@ -14,8 +12,9 @@ import (
 // +k8s:openapi-gen=true
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:shortName=clobs;clobs
-// +kubebuilder:resource:scope=Cluster
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="OpenTelemetry",type="string",JSONPath=".status.opentelemetry"
+// +kubebuilder:printcolumn:name="Tempo",type="string",JSONPath=".status.tempo"
 // +operator-sdk:csv:customresourcedefinitions:displayName="Cluster Observability"
 type ClusterObservability struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -37,26 +36,56 @@ type ClusterObservabilityList struct {
 }
 
 type ClusterObservabilitySpec struct {
-	// Storage defines the storage for the capabilities that require a storage.
-	Storage StorageSpec `json:"storage,omitempty"`
-
 	// Capabilities defines the observability capabilities.
 	// Each capability has to be enabled explicitly.
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Capabilities",order=2
 	Capabilities *CapabilitiesSpec `json:"capabilities,omitempty"`
 }
 
 // ClusterObservabilityStatus defines the observed state of ClusterObservability.
-type ClusterObservabilityStatus struct{}
+type ClusterObservabilityStatus struct {
+	// OpenTelemetry defines the status of the OpenTelemetry capability.
+	// The value is in the form of instance namespace/name (version)
+	// +optional
+	OpenTelemetry string `json:"opentelemetry,omitempty"`
+	// Tempo defines the status of the Tempo capability.
+	// The value is in the form of instance namespace/name (version)
+	// +optional
+	Tempo string `json:"tempo,omitempty"`
 
-// StorageSpec defines the storage.
-type StorageSpec struct {
-	Secret SecretSpec `json:"secret,omitempty"`
+	// Conditions provide status information about the instance.
+	// +listType=atomic
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
-// SecretSpec defines the secret for the storage.
-type SecretSpec struct {
-	// Name is the name of the secret for the storage.
-	Name string `json:"name,omitempty"`
+// TLSSpec is the TLS configuration.
+// +kubebuilder:validation:XValidation:rule="(has(self.keySecret) && has(self.certSecret)) || (!has(self.keySecret) && !has(self.certSecret))",message="KeySecret and CertSecret must be set together"
+type TLSSpec struct {
+	// CAConfigMap is the name of a ConfigMap containing a CA certificate (e.g. service-ca.crt).
+	//
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors="urn:alm:descriptor:io.kubernetes:ConfigMap",displayName="CA ConfigMap"
+	CAConfigMap *ConfigMapKeySelector `json:"caConfigMap,omitempty"`
+
+	// CertSecret is the name of a Secret containing a certificate (e.g. tls.crt).
+	//
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors="urn:alm:descriptor:io.kubernetes:Secret",displayName="Certificate Secret"
+	CertSecret *SecretKeySelector `json:"certSecret,omitempty"`
+
+	// KeySecret is the name of a Secret containing a private key (e.g. tls.key).
+	//
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors="urn:alm:descriptor:io.kubernetes:Secret",displayName="Certificate Secret"
+	KeySecret *SecretKeySelector `json:"keySecret,omitempty"`
+
+	// MinVersion defines the minimum acceptable TLS version.
+	//
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Min TLS Version"
+	MinVersion string `json:"minVersion,omitempty"`
 }
 
 // CapabilitiesSpec defines the observability capabilities.
@@ -66,20 +95,14 @@ type CapabilitiesSpec struct {
 	// +optional
 	// +kubebuilder:validation:Optional
 	Tracing TracingSpec `json:"tracing,omitempty"`
-
-	// OpenTelemetry defines the OpenTelemetry capabilities.
-	// +optional
-	// +kubebuilder:validation:Optional
-	OpenTelemetry OpenTelemetrySpec `json:"opentelemetry,omitempty"`
 }
 
 // CommonCapabilitiesSpec defines the common capabilities.
 type CommonCapabilitiesSpec struct {
-	// Enabled indicates whether the capability is enabled and it operator should deploy an instance.
+	// Enabled indicates whether the capability is enabled and whether the operator should deploy an instance.
 	// By default, it is set to false.
 	// +optional
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:default=false
 	Enabled bool `json:"enabled,omitempty"`
 
 	// Operators defines the operators installation for the capability.
@@ -94,5 +117,5 @@ type OperatorsSpec struct {
 	// When the capability is enabled, the install is set to true, otherwise it is set to false.
 	// +optional
 	// +kubebuilder:validation:Optional
-	Install bool `json:"install,omitempty"`
+	Install *bool `json:"install,omitempty"`
 }
