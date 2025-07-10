5ae858fa0e2390d4a48f70e8b1334e39eb7003df
Pavol Loffay <p.loffay@gmail.com>
2025-07-10 19:21:03 +0200

    chore: add e2e test
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/controllers/observability/observability_controller.go
+++ pkg/controllers/observability/observability_controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -14,10 +15,15 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	tempov1alpha1 "github.com/grafana/tempo-operator/api/tempo/v1alpha1"
 	otelv1beta1 "github.com/open-telemetry/opentelemetry-operator/apis/v1beta1"
@@ -54,10 +60,15 @@ const (
 // +kubebuilder:rbac:groups=tempo.grafana.com,resources=prod,resourceNames=traces,verbs=create
 
 type clusterObservabilityController struct {
-	client  client.Client
-	scheme  *runtime.Scheme
-	logger  logr.Logger
-	Options Options
+	client          client.Client
+	scheme          *runtime.Scheme
+	logger          logr.Logger
+	Options         Options
+	controller      controller.TypedController[reconcile.Request]
+	cache           cache.Cache
+	discoveryClient *discovery.DiscoveryClient
+	watchOTELcol    *sync.Once
+	watchTempo      *sync.Once
 }
 
 var _ reconcile.TypedReconciler[reconcile.Request] = (*clusterObservabilityController)(nil)
@@ -130,6 +141,29 @@ func (o clusterObservabilityController) Reconcile(ctx context.Context, request r
 		}
 	}
 
+	groups, err := o.discoveryClient.ServerGroups()
+	if err != nil {
+		o.logger.Error(err, "Failed to get server groups / CDRs")
+		return ctrl.Result{}, fmt.Errorf("failed to get server groups: %w", err)
+	}
+	for _, group := range groups.Groups {
+		if group.Name == "opentelemetry.io" {
+			o.watchOTELcol.Do(func() {
+				if err := o.controller.Watch(source.Kind[client.Object](o.cache, &otelv1beta1.OpenTelemetryCollector{}, handler.EnqueueRequestsFromMapFunc(o.triggerReconcile))); err != nil {
+					o.logger.Error(err, "Failed to watch OpenTelemetryCollector resources")
+				}
+			})
+		}
+
+		if group.Name == "tempo.grafana.com" {
+			o.watchTempo.Do(func() {
+				if err := o.controller.Watch(source.Kind[client.Object](o.cache, &otelv1beta1.OpenTelemetryCollector{}, handler.EnqueueRequestsFromMapFunc(o.triggerReconcile))); err != nil {
+					o.logger.Error(err, "Failed to watch TempoStack resources")
+				}
+			})
+		}
+	}
+
 	// We have a deletion, short circuit and let the deletion happen
 	if instance.ObjectMeta.DeletionTimestamp != nil {
 		if controllerutil.ContainsFinalizer(instance, finalizerName) {
@@ -145,6 +179,26 @@ func (o clusterObservabilityController) Reconcile(ctx context.Context, request r
 	}
 
 	return o.updateStatus(ctx, instance, nil), nil
+}
+
+func (o clusterObservabilityController) triggerReconcile(ctx context.Context, _ client.Object) []reconcile.Request {
+	instances := &obsv1alpha1.ClusterObservabilityList{}
+	listOps := &client.ListOptions{}
+	err := o.client.List(ctx, instances, listOps)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(instances.Items))
+	for i, item := range instances.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      item.GetName(),
+				Namespace: item.GetNamespace(),
+			},
+		}
+	}
+	return requests
 }
 
 func (o clusterObservabilityController) getInstance(ctx context.Context, req ctrl.Request) (*obsv1alpha1.ClusterObservability, error) {
@@ -240,18 +294,25 @@ func RegisterWithManager(mgr ctrl.Manager, opts Options) error {
 		return nil
 	}
 
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(mgr.GetConfig())
+	if err != nil {
+		return fmt.Errorf("failed to create discovery client: %w", err)
+	}
+
 	controller := &clusterObservabilityController{
-		client:  mgr.GetClient(),
-		scheme:  mgr.GetScheme(),
-		logger:  logger,
-		Options: opts,
+		client:          mgr.GetClient(),
+		scheme:          mgr.GetScheme(),
+		logger:          logger,
+		Options:         opts,
+		watchOTELcol:    &sync.Once{},
+		watchTempo:      &sync.Once{},
+		discoveryClient: discoveryClient,
+		cache:           mgr.GetCache(),
 	}
 
 	ctrlBuilder := ctrl.NewControllerManagedBy(mgr).
 		For(&obsv1alpha1.ClusterObservability{}).
 		Owns(&olmv1alpha1.Subscription{}).
-		Owns(&otelv1beta1.OpenTelemetryCollector{}).
-		Owns(&tempov1alpha1.TempoStack{}).
 		Owns(&corev1.Secret{}).
 		Owns(&corev1.Namespace{}).
 		Owns(&uiv1alpha1.UIPlugin{}).
@@ -259,8 +320,7 @@ func RegisterWithManager(mgr ctrl.Manager, opts Options) error {
 		Owns(&rbacv1.ClusterRoleBinding{}).
 		Named("cluster-observability")
 
-	_, err := ctrlBuilder.Build(controller)
-
+	controller.controller, err = ctrlBuilder.Build(controller)
 	if err != nil {
 		return err
 	}
--- test/e2e/cluster_observability_test.go
+++ test/e2e/cluster_observability_test.go
@@ -2,18 +2,29 @@ package e2e
 
 import (
 	"context"
+	_ "embed"
+	"regexp"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
 	"gotest.tools/v3/assert"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/utils/ptr"
 
 	obsv1alpha1 "github.com/rhobs/observability-operator/pkg/apis/observability/v1alpha1"
+)
+
+var (
+	//go:embed minio.yaml
+	minioManifests string
 )
 
 func TestClusterObservabilityController(t *testing.T) {
@@ -33,19 +44,57 @@ func testClusterObservabilityInstallOperators(t *testing.T) {
 	ctx := context.Background()
 	name := "test-cluster-observability-lifecycle"
 
+	for _, doc := range strings.Split(minioManifests, "---") {
+		if strings.TrimSpace(doc) == "" {
+			continue
+		}
+
+		// Create an unstructured object to decode the YAML into
+		obj := &unstructured.Unstructured{}
+
+		// Use the YAML decoder to convert the YAML string into the unstructured object
+		decoder := yamlutil.NewYAMLOrJSONDecoder(strings.NewReader(doc), 100)
+		err := decoder.Decode(&obj)
+		require.NoError(t, err)
+
+		// Use the client to create the object in the Kubernetes cluster
+		err = f.K8sClient.Create(context.Background(), obj)
+		require.NoError(t, err)
+		f.CleanUp(t, func() { f.K8sClient.Delete(ctx, obj) })
+	}
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "minio",
+			Namespace: f.OperatorNamespace,
+		},
+		Data: map[string][]byte{
+			"endpoint":          []byte("http://minio.minio.svc:9000"),
+			"bucket":            []byte("tempo"),
+			"access_key_id":     []byte("tempo"),
+			"access_key_secret": []byte("supersecret"),
+		},
+	}
+	err := f.K8sClient.Create(ctx, secret)
+	require.NoError(t, err)
+	f.CleanUp(t, func() { f.K8sClient.Delete(ctx, secret) })
+
 	// Create ClusterObservability resource
 	clusterObs := &obsv1alpha1.ClusterObservability{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
 		},
 		Spec: obsv1alpha1.ClusterObservabilitySpec{
+			Storage: obsv1alpha1.StorageSpec{
+				Secret: obsv1alpha1.SecretSpec{
+					Name: "minio",
+					Type: "s3",
+				},
+			},
 			Capabilities: &obsv1alpha1.CapabilitiesSpec{
 				Tracing: obsv1alpha1.TracingSpec{
 					CommonCapabilitiesSpec: obsv1alpha1.CommonCapabilitiesSpec{
-						Enabled: false,
-						Operators: obsv1alpha1.OperatorsSpec{
-							Install: ptr.To(true),
-						},
+						Enabled: true,
 					},
 				},
 			},
@@ -57,7 +106,7 @@ func testClusterObservabilityInstallOperators(t *testing.T) {
 	})
 
 	// Create the resource
-	err := f.K8sClient.Create(ctx, clusterObs)
+	err = f.K8sClient.Create(ctx, clusterObs)
 	assert.NilError(t, err, "Failed to create ClusterObservability resource")
 
 	// Verify resource exists
@@ -65,8 +114,19 @@ func testClusterObservabilityInstallOperators(t *testing.T) {
 	err = f.K8sClient.Get(ctx, types.NamespacedName{Name: name}, &createdClusterObs)
 	assert.NilError(t, err, "Failed to get ClusterObservability resource")
 
-	// Verify spec is correct
-	assert.Equal(t, createdClusterObs.Spec.Capabilities.Tracing.Enabled, false)
+	// TODO implement a data test - report traces and check the collector if export was successful
+	err = wait.PollUntilContextTimeout(ctx, 1*time.Second, 60*time.Second, true, func(ctx context.Context) (bool, error) {
+		var instance obsv1alpha1.ClusterObservability
+		err := f.K8sClient.Get(ctx, types.NamespacedName{Name: name}, &instance)
+		if apierrors.IsNotFound(err) {
+			return false, nil
+		}
+		r, _ := regexp.Compile("cluster-observability2/coo ([0-9].*)")
+		if r.MatchString(instance.Status.Tempo) {
+			return true, nil
+		}
+		return false, nil
+	})
 
 	// Delete the resource
 	err = f.K8sClient.Delete(ctx, &createdClusterObs)
