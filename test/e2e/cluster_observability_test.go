cdb9be21dcbfeafdbe4e7b7955db651277cb6c95
Pavol Loffay <p.loffay@gmail.com>
2025-07-14 13:32:36 +0200

    chore: update e2e test
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- test/e2e/cluster_observability_test.go
+++ test/e2e/cluster_observability_test.go
@@ -3,6 +3,7 @@ package e2e
 import (
 	"context"
 	_ "embed"
+	"fmt"
 	"regexp"
 	"strings"
 	"testing"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gotest.tools/v3/assert"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,8 +25,10 @@ import (
 )
 
 var (
-	//go:embed minio.yaml
+	//go:embed traces_minio.yaml
 	minioManifests string
+	//go:embed traces_verify.yaml
+	verifyTracesManifests string
 )
 
 func TestClusterObservabilityController(t *testing.T) {
@@ -114,15 +118,107 @@ func testClusterObservabilityInstallOperators(t *testing.T) {
 	err = f.K8sClient.Get(ctx, types.NamespacedName{Name: name}, &createdClusterObs)
 	assert.NilError(t, err, "Failed to get ClusterObservability resource")
 
-	// TODO implement a data test - report traces and check the collector if export was successful
-	err = wait.PollUntilContextTimeout(ctx, 1*time.Second, 60*time.Second, true, func(ctx context.Context) (bool, error) {
+	// Wait for Tempo to be ready
+	err = wait.PollUntilContextTimeout(ctx, 1*time.Second, 5*time.Minute, true, func(ctx context.Context) (bool, error) {
 		var instance obsv1alpha1.ClusterObservability
 		err := f.K8sClient.Get(ctx, types.NamespacedName{Name: name}, &instance)
 		if apierrors.IsNotFound(err) {
 			return false, nil
 		}
-		r, _ := regexp.Compile("cluster-observability2/coo ([0-9].*)")
+		r, _ := regexp.Compile(`cluster-observability/coo \([0-9]+.*\)`)
+		fmt.Printf("Tempo status: %s\n", instance.Status.Tempo)
 		if r.MatchString(instance.Status.Tempo) {
+			return true, nil
+		}
+		return false, nil
+	})
+	require.NoError(t, err)
+	// Wait for collector to be ready
+	err = wait.PollUntilContextTimeout(ctx, 1*time.Second, 5*time.Minute, true, func(ctx context.Context) (bool, error) {
+		var instance obsv1alpha1.ClusterObservability
+		err := f.K8sClient.Get(ctx, types.NamespacedName{Name: name}, &instance)
+		if apierrors.IsNotFound(err) {
+			return false, nil
+		}
+		r, _ := regexp.Compile(`cluster-observability/coo \([0-9]+.*\)`)
+		fmt.Printf("OTEL status: %s\n", instance.Status.OpenTelemetry)
+		if r.MatchString(instance.Status.OpenTelemetry) {
+			return true, nil
+		}
+		return false, nil
+	})
+	require.NoError(t, err)
+
+	job := batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "generate-traces-grpc",
+			Namespace: "cluster-observability",
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "telemetrygen",
+							Image: "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen:v0.129.0",
+							Args: []string{
+								"traces",
+								"--otlp-endpoint=coo-collector.cluster-observability.svc.cluster.local:4317",
+								"--service=grpc",
+								"--otlp-insecure",
+								"--traces=10",
+							},
+						},
+					},
+					RestartPolicy: "Never",
+				},
+			},
+		},
+	}
+	err = f.K8sClient.Create(ctx, &job)
+	require.NoError(t, err)
+
+	// Check if the job succeeded
+	err = wait.PollUntilContextTimeout(ctx, 1*time.Second, 1*time.Minute, true, func(ctx context.Context) (bool, error) {
+		var job batchv1.Job
+		err := f.K8sClient.Get(ctx, types.NamespacedName{Name: "generate-traces-grpc", Namespace: "cluster-observability"}, &job)
+		if apierrors.IsNotFound(err) {
+			return false, nil
+		}
+		if job.Status.Succeeded > 0 {
+			return true, nil
+		}
+		return false, nil
+	})
+	require.NoError(t, err)
+
+	for _, doc := range strings.Split(verifyTracesManifests, "---") {
+		if strings.TrimSpace(doc) == "" {
+			continue
+		}
+
+		// Create an unstructured object to decode the YAML into
+		obj := &unstructured.Unstructured{}
+
+		// Use the YAML decoder to convert the YAML string into the unstructured object
+		decoder := yamlutil.NewYAMLOrJSONDecoder(strings.NewReader(doc), 100)
+		err := decoder.Decode(&obj)
+		require.NoError(t, err)
+
+		// Use the client to create the object in the Kubernetes cluster
+		err = f.K8sClient.Create(context.Background(), obj)
+		require.NoError(t, err)
+		f.CleanUp(t, func() { f.K8sClient.Delete(ctx, obj) })
+	}
+
+	// Check if the verify traces job succeeded
+	err = wait.PollUntilContextTimeout(ctx, 1*time.Second, 1*time.Minute, true, func(ctx context.Context) (bool, error) {
+		var job batchv1.Job
+		err := f.K8sClient.Get(ctx, types.NamespacedName{Name: "verify-traces-traceql-grpc", Namespace: "cluster-observability"}, &job)
+		if apierrors.IsNotFound(err) {
+			return false, nil
+		}
+		if job.Status.Succeeded > 0 {
 			return true, nil
 		}
 		return false, nil
